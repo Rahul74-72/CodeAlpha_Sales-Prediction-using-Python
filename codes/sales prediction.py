f149352b722dbf1fd7ea072625206745eaeb5e9a
Rahul74-72 <72rahuljat@gmail.com>
2025-05-21 11:23:23 +0530

    Add files via upload

---
+++ codes/sales prediction.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+
+df = pd.read_csv("C:/Users/HP/Desktop/project/archive/Advertising.csv")
+
+X = df[['TV', 'Radio', 'Newspaper']]
+y = df['Sales']
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse:.2f}")
+
+new_spend = [[3500, 50, 10]]
+predicted_sales = model.predict(new_spend)
+
+print(f"Predicted Sales for TV=3500, Radio=50, Newspaper=10: {predicted_sales[0]:.2f}")
+
+plt.figure(figsize=(8, 5))
+plt.scatter(y_test, y_pred, alpha=0.5)
+plt.xlabel("Actual Sales")
+plt.ylabel("Predicted Sales")
+plt.title("Actual vs Predicted Sales")
+plt.grid(True)
+plt.show()
